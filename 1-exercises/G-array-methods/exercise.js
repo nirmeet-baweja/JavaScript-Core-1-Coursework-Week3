266486b9845b409d5f029f947db10907b410cefc
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-24 00:06:36 +0000

    Exercise 7 Completed

--- 1-exercises/G-array-methods/exercise.js
+++ 1-exercises/G-array-methods/exercise.js
@@ -4,15 +4,15 @@
 */
 
 var numbers = [3, 2, 1];
-var sortedNumbers; // complete this statement
+var sortedNumbers = numbers.sort(); // complete this statement
 
-/* 
+/*
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
 
 console.log(sortedNumbers);
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   [1, 2, 3]
