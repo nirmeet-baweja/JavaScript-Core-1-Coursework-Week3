c6758556a26da6f3b337e1d0b78c65f8f236ad29
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-24 00:29:43 +0000

    Exercise 9 Completed
    There is an error in the test function call in test 2. The test should be carried out with 'result' being passed as a parameter to the function instead of 'story'.

--- 1-exercises/I-string-replace/exercise.js
+++ 1-exercises/I-string-replace/exercise.js
@@ -1,19 +1,22 @@
-/* 
+/*
   You are given a sentence contains a story.
 
-  Current it says 
+  Current it says
 
   "I like dogs. One day I went to the park and I saw 10 dogs. It was a great day."
 
-  Change the story using .replace() so that it says 
+  Change the story using .replace() so that it says
 
   "I like cats. One night I went to the park and I saw 100000 cats. It was a brilliant night."
 */
 
-let story =
-  "I like dogs. One day I went to the park and I saw 10 dogs. It was a great day.";
+let story = "I like dogs. One day I went to the park and I saw 10 dogs. It was a great day.";
 
-let result = story.replace("", "");
+/*
+ * It can be done in two ways either we replace word by word, or we can replace the old story with the new story.
+ *
+ */
+let result = story.replace("I like dogs. One day I went to the park and I saw 10 dogs. It was a great day.", "I like cats. One night I went to the park and I saw 100000 cats. It was a brilliant night.");
 
 /* EXPECTED OUTPUT */
 
@@ -41,6 +44,6 @@ test(
 
 test(
   "2. The result of the replace is correct",
-  story,
+  result,
   "I like cats. One night I went to the park and I saw 100000 cats. It was a brilliant night."
 );
