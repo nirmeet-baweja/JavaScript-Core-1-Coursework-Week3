3a42c888812268c4ee2968ac97c80817232aea8c
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-24 00:01:03 +0000

    Exercise 6 Completed

--- 1-exercises/F-array-forEach/README.md
+++ 1-exercises/F-array-forEach/README.md
@@ -32,7 +32,7 @@ names.forEach(function(name, index) {
 
 This logs each name to the console as hoped, but we notice that the names are not formatted correctly. You might be tempted to format the name inside of the `forEach` function.
 
-However, it is good practise to write small functions with a single responsibility. So instead, we can write a `formatName` function (which we can re-use in other places) and pass it to `.map()` before calling `.forEach()`.
+However, it is good practice to write small functions with a single responsibility. So instead, we can write a `formatName` function (which we can re-use in other places) and pass it to `.map()` before calling `.forEach()`.
 
 ```js
 function formatName(name) {
--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -1,13 +1,33 @@
-/* 
+/*
   Using .forEach() print the numbers 1 to 15, with some exceptions:
-  - For multiples of 3 print “Fizz” instead of the number 
-  - For the multiples of 5 print “Buzz”. 
+  - For multiples of 3 print “Fizz” instead of the number
+  - For the multiples of 5 print “Buzz”.
   - For numbers which are multiples of both 3 and 5 print “FizzBuzz”
 
   An array with numbers 1-15 has been provided.
 */
 
+function printNum(num)
+{
+  // check if num is divisible by 3 or 5
+  if((num % 3 === 0) || (num % 5 === 0))
+  {
+    //check if num is divisible by 3
+    if(num % 3 === 0)
+      console.log("Fizz");
+    else
+      // check if num is divisible by 5
+      if(num % 5 === 0)
+        console.log("Buzz");
+      else // if we reach this else, it means num is divisible by both 3 and 5
+        console.log("FizzBuzz");
+  }
+  else
+    console.log(num);
+}
+
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+arr.forEach(printNum);
 
 /* EXPECTED OUTPUT */
 
