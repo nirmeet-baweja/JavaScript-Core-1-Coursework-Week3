7f971412cc6fd698a906facecbc397a38255ac87
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-28 13:09:17 +0000

    Challenge 2 Completed

--- 2-mandatory/2-bush-berries.js
+++ 2-mandatory/2-bush-berries.js
@@ -9,19 +9,27 @@
 
   Create a function which checks if the bush has ALL PINK berries and is safe for the astronauts to eat from the bush.
   Use the tests to confirm which message to return
-  
+
   This exercise can be solved in a few different ways. One way might include the array methods
-  .some() and .every(). 
-  
-  The .some() method tests to see if some of the values (at least 1) in an array 
-  match what you're looking for and returns true or false. 
-  
-  The .every() method will only return true if all values match watch you're looking for. 
-  
+  .some() and .every().
+
+  The .some() method tests to see if some of the values (at least 1) in an array
+  match what you're looking for and returns true or false.
+
+  The .every() method will only return true if all values match watch you're looking for.
+
   Let's first look at an example that will teach you how to use these methods.
 */
 
-function bushChecker() {}
+function bushChecker(bush)
+{
+  hasAllPinkBerries = bush.every(berry => berry === "pink");
+
+  if(hasAllPinkBerries)
+    return "Bush is safe to eat from";
+  else
+    return "Toxic! Leave bush alone!";
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
