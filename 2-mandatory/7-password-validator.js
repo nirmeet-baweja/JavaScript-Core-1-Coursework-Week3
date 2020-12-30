60a19e632bf28bc875d4d41ee21577befe9289bb
nirmeet-baweja <73722230+nirmeet-baweja@users.noreply.github.com>
2020-12-30 01:11:49 +0000

    Challenge 7 Completed

--- 2-mandatory/7-password-validator.js
+++ 2-mandatory/7-password-validator.js
@@ -1,7 +1,7 @@
-/* 
+/*
 Password Validation
 
-Write a program that should check if each password in an array 
+Write a program that should check if each password in an array
 contains a valid password (see below for password criteria) and return a
 new array with true or false booleans for whether that password was valid or not.
 
@@ -12,7 +12,7 @@ To be valid, a password must:
 - Have at least one number (0-9)
 - Have at least one non-alphanumeric symbol ("!", "#", "$", "%", ".", "*", "&")
 
-Passwords must not be any previous password in the passwords array. 
+Passwords must not be any previous password in the passwords array.
 
 Example 1:
 PreviousPassword = ["fhD8!yrjj", "ttkTu.wer3", "dvyyeyY!5", "qwbfj76%", "tytT3729."];
@@ -22,8 +22,31 @@ PasswordValidationResult=  [false, false, false, false, true]
 
 */
 
-function validatePasswords(passwords) {
+function validatePasswords(passwords)
+{
+  /*
+    RegEx	Description
+    ^	The password string will start this way
+    (?=.*[a-z]) The string must contain at least 1 lowercase alphabetical character
+    (?=.*[A-Z])	The string must contain at least 1 uppercase alphabetical character
+    (?=.*[0-9])	The string must contain at least 1 numeric character
+    (?=.*[!@#$%^&*])	The string must contain at least one special character, but we are escaping reserved RegEx characters to avoid conflict
+    (?=.{8,})	The string must be eight characters or longer
+*/
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{5,})/;
 
+  let passwordValidity = [];
+
+  for(let i =0; i < passwords.length; i++)
+  {
+    let tempPasswords = passwords.slice(0, i+1);
+    let isFirstOccurrence = (tempPasswords.indexOf(passwords[i]) === i);
+    let isValidPassword = regex.test(passwords[i]) && isFirstOccurrence;
+    passwordValidity.push(isValidPassword);
+  }
+
+  console.log(`The result array is ${passwordValidity}`);
+  return passwordValidity;
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
